5e1f2567f453535d5be0c235c1a8ab4f774c765b
Sebastian Paciorek <sebastian.paciorek@emerson.com>
2022-09-16 11:38:05 +0200

    Add created field to user

--- app/src/main/java/com/example/roommigrations/User.kt
+++ app/src/main/java/com/example/roommigrations/User.kt
@@ -9,4 +9,6 @@ data class User(
     @PrimaryKey(autoGenerate = false)
     val email: String,
     val username: String,
+    @ColumnInfo(name = "created", defaultValue = "0")
+    val created: Long = System.currentTimeMillis()
 )
--- app/src/main/java/com/example/roommigrations/UserDatabase.kt
+++ app/src/main/java/com/example/roommigrations/UserDatabase.kt
@@ -6,7 +6,10 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [User::class],
-    version = 1,
+    version = 2,
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2)
+    ]
 )
 abstract class UserDatabase : RoomDatabase() {
     abstract val dao: UserDao
