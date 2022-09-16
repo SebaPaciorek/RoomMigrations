c4d4c2e63f7073063658a3f71be556b84ea4c734
Sebastian Paciorek <sebastian.paciorek@emerson.com>
2022-09-16 11:59:34 +0200

    Rename created field from created to createdAt for user table

--- app/src/main/java/com/example/roommigrations/UserDatabase.kt
+++ app/src/main/java/com/example/roommigrations/UserDatabase.kt
@@ -2,15 +2,21 @@ package com.example.roommigrations
 
 import androidx.room.AutoMigration
 import androidx.room.Database
+import androidx.room.RenameColumn
 import androidx.room.RoomDatabase
+import androidx.room.migration.AutoMigrationSpec
 
 @Database(
     entities = [User::class],
-    version = 2,
+    version = 3,
     autoMigrations = [
-        AutoMigration(from = 1, to = 2)
+        AutoMigration(from = 1, to = 2),
+        AutoMigration(from = 2, to = 3, spec = UserDatabase.MigrationFrom2To3::class)
     ]
 )
 abstract class UserDatabase : RoomDatabase() {
     abstract val dao: UserDao
+
+    @RenameColumn(tableName = "User", fromColumnName = "created", toColumnName = "createdAt")
+    class MigrationFrom2To3 : AutoMigrationSpec
 }
